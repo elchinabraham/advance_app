523ac71e1e9264283d3317b118607c85a93e533b
Elchin Abraham <elchinibrahimli8@gmail.com>
2022-05-04 17:11:14 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:advance_app/providers/great_places.dart';
+import 'package:advance_app/screens/places_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+      ),
+    );
+  }
+}
