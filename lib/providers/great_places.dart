f459eed746b430731d50badbe1c5448731e7e443
Elchin Abraham <elchinibrahimli8@gmail.com>
2022-05-11 16:36:56 +0400

    2022.05.11 16:30

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,5 +1,7 @@
+import 'dart:ffi';
 import 'dart:io';
 
+import 'package:advance_app/helper/db_helper.dart';
 import 'package:flutter/cupertino.dart';
 import '../models/place.dart';
 
@@ -21,6 +23,35 @@ class GreatPlaces with ChangeNotifier {
       location: PlaceLocation(address: '', latitude: 12, longitude: 12),
     );
     _items.add(newPlace);
+    notifyListeners();
+
+    DBHelper.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+      },
+    );
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    print(dataList);
+    _items = dataList
+        .map(
+          (e) => Place(
+            id: e['id'],
+            title: e['title'],
+            image: File(e['image']),
+            location: PlaceLocation(
+              address: '',
+              latitude: 0,
+              longitude: 0,
+            ),
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
